55bb2d3242a7aa1cc8136c15a3d322b5cd81bcbd
Oscar <oscar.larsen@edu.newton.se>
2025-05-15 17:15:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace EtteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            string XMLpath = Directory.GetCurrentDirectory() + "\\test.xml";
+
+
+
+        }
+    }
+}

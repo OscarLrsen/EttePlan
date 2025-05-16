2b335c514eda3085db14ca352d453233d056c100
Oscar <oscar.larsen@edu.newton.se>
2025-05-16 12:41:27 +0200

    Skriver värdet till text fil,  README och lagt kommentarer.

--- Functions.cs
+++ Functions.cs
@@ -6,27 +6,40 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Xml;
 using System.Diagnostics;
+using System.IO;
 
 namespace EtteTest
 {
     class Functions
     {
-         public void ReadSingleAttribute()
-         {
-            XElement xelement = XElement.Load("gentext.xml");
+        public void ReadSingleAttribute()
+        {
+            XElement xelement = XElement.Load("gentext.xml"); // XElement läser av XML fil
 
-            var gentext = xelement.Descendants("trans-unit");
+            var gentext = xelement.Descendants("trans-unit"); //Letar upp alla element i Gentext som heter <trans-unit>
 
-            var data = from target in gentext
-                         where target != null &&
-                               target.Element("target").Value.Contains("Filen")
+            var data = from target in gentext //Går igenom alla element i gentext
+                       where target != null &&       //Kollar så inte elementet är tomt
+                               target.Element("target").Value.Contains("Filen")   //Kollar om target elementet innehåller "Filen"
                        select target;
 
-            foreach(var item in data)
-            {
-                Console.WriteLine(item.Value);
-            }
 
+
+            string docPath = @"C:\Users\oscar\Desktop\Jobb\EttEtest\EtteTest"; //Sökvägen till där textfilen ska sparas
+            string filePath = Path.Combine(docPath, "Textfil.txt"); //Sökvägen och namnet på textfilen
+
+            using (StreamWriter writetext = new StreamWriter(filePath)) //Skapar en textfil
+            {
+                foreach (var item in data) 
+                {
+                    string value = item.Value;
+
+                    Console.WriteLine(item.Value); //Skriver ut värdet i konsolen
+
+                    writetext.WriteLine(item.Value); //Skriver ut värdet i en textfil
+
+                }
+            }
 
 
         }
